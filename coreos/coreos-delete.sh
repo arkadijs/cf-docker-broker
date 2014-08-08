ba40b60314e610a9b0c551c0f6fa0b002b017ac6
Arkadi Shishlov <arkadi.shishlov@gmail.com>
2014-08-09 00:10:03 +0300

    Release IP and delete Target Instances

--- coreos/coreos-delete.sh
+++ coreos/coreos-delete.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 . ./coreos-common.sh
-
+set +e
+gcutil releaseaddress -f --region=$region $reserved_ip_name
+gcutil deletetargetinstance -f --zone=$zone $(coreos_tis)
 gcutil deleteinstance -f --delete_boot_pd --zone=$zone $(coreos_nodes)
 gcutil deletedisk -f --zone=$zone $(coreos_swap_disks)
